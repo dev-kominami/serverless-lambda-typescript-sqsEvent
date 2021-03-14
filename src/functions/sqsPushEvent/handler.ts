3debd1d9cf6759d2714300cc58e4c55de5caa3a6
Takashi <dev.kominami@gmail.com>
2021-03-14 16:50:23 +0900

    aws-sdkを追加

--- src/functions/sqsPushEvent/handler.ts
+++ src/functions/sqsPushEvent/handler.ts
@@ -2,8 +2,13 @@ import 'source-map-support/register';
 
 import { middyfy } from '@libs/lambda';
 
+import AWS from 'aws-sdk';
+const sqs = new AWS.SQS()
+
 const pushEvent = async (event) => {
   console.log(event);
+  console.log(AWS);
+  console.log(sqs);
 }
 
 export const main = middyfy(pushEvent);
