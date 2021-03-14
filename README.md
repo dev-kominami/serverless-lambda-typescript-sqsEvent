a40501c7dcef6ac66f24a50dcddb22a3afdde1c0
Takashi <dev.kominami@gmail.com>
2021-03-14 17:32:09 +0900

    sqs send messageを実装。

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Serverless - AWS Node.js Typescript SQS Event
 SQSへのメッセージ追加と、
 SQSからイベントをトリガーするLambdaのサンプル
+
+## ローカルで実行する場合
+### sqsPushEvent
+ローカルで実行する場合は環境変数でsqs queueのURLを指定して実行する
+
+```
+sls invoke local -f sqsPushEvent -p src/functions/sqsPushEvent/mock.json --aws-profile {プロファイル} -e sqsQuereUrl={sqsのURL}
+```
--- src/functions/sqsPushEvent/handler.ts
+++ src/functions/sqsPushEvent/handler.ts
@@ -5,10 +5,21 @@ import { middyfy } from '@libs/lambda';
 import AWS from 'aws-sdk';
 const sqs = new AWS.SQS()
 
+const url = process.env.sqsQuereUrl;
+
 const pushEvent = async (event) => {
   console.log(event);
-  console.log(AWS);
-  console.log(sqs);
+  const param: AWS.SQS.SendMessageRequest = {
+    MessageBody: JSON.stringify(event),
+    QueueUrl: url,
+    MessageDeduplicationId: "dedup_2",
+    MessageGroupId: "group_2"
+  }
+
+  sqs.sendMessage(param, function(err, data) {
+    if (err) console.log(err);
+    else console.log(data);
+  })
 }
 
 export const main = middyfy(pushEvent);
